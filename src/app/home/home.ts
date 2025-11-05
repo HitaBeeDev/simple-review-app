f3888c29c4f6d29113566e0b758d67f4342dce1d
HitaBeeDev <anahita.amiri103@gmail.com>
2025-11-05 16:11:51 +0100

    ui

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -7,6 +7,8 @@ type Card = {
   name: string;
   review: string;
   product: string;
+  rate: number;
+  role: string;
 };
 
 @Component({
@@ -23,18 +25,24 @@ export class Home {
       name: 'Alice Johnson',
       review: 'Amazing app! The UI is clean and intuitive.',
       product: 'ReviewPro App',
+      rate: 5,
+      role: 'Designer',
     },
     {
       id: 2,
       name: 'Mark Evans',
       review: 'Very useful and easy to navigate.',
       product: 'TaskTracker',
+      rate: 4,
+      role: 'Developer',
     },
     {
       id: 3,
       name: 'Mark Evans',
       review: 'Very useful and easy to navigate.',
       product: 'TaskTracker',
+      rate: 4,
+      role: 'Developer',
     },
   ];
 
