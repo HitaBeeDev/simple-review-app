1a145e1344dd9008f37ab83f87ffda40df5aecd4
HitaBeeDev <anahita.amiri103@gmail.com>
2025-11-05 15:13:40 +0100

    some ui added

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReviewForm } from '../review-form/review-form';
-import { ReviewList } from '../review-list/review-list';
 import { RouterLink } from '@angular/router';
+
+type Card = {
+  id: number;
+  name: string;
+  review: string;
+  product: string;
+};
 
 @Component({
   selector: 'app-home',
-  imports: [ReviewForm, ReviewList, RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
 })
-export class Home {}
+export class Home {
+  cards: Card[] = [
+    {
+      id: 1,
+      name: 'Alice Johnson',
+      review: 'Amazing app! The UI is clean and intuitive.',
+      product: 'ReviewPro App',
+    },
+    {
+      id: 2,
+      name: 'Mark Evans',
+      review: 'Very useful and easy to navigate.',
+      product: 'TaskTracker',
+    },
+    {
+      id: 3,
+      name: 'Mark Evans',
+      review: 'Very useful and easy to navigate.',
+      product: 'TaskTracker',
+    },
+  ];
+
+  selectedIndex: number | null = null;
+}
